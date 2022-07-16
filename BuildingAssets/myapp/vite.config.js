cb1616ded24caf5531de5874c8d47a344b3b7fcb
Asif Bin Kabir <asifbinkabircse@gmail.com>
2022-07-16 19:04:04 +0600

    Laravel building assets practice

---
+++ BuildingAssets/myapp/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/query.css',
+                'resources/css/index.css',
+                'resources/js/index.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
